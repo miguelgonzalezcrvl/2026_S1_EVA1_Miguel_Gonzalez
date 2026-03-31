b62ce0bca38469740e77d35d467050cce96686f2
Miguel González <migu.gonzalezc@duocuc.cl>
2026-03-31 10:13:37 -0300

    Mi primer commit de la EVA1

---
+++ src/main/java/cl/duoc/miguel_gonzalez/MiguelGonzalezApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.miguel_gonzalez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiguelGonzalezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiguelGonzalezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/miguel_gonzalez/MiguelGonzalezApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.miguel_gonzalez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiguelGonzalezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
