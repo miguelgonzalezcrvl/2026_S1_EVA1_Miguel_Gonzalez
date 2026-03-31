2cc8262132ca35de16b3411ce9be530f0251e037
Miguel González <migu.gonzalezc@duocuc.cl>
2026-03-31 12:03:08 -0300

    avance 3

--- src/main/java/cl/duoc/miguel_gonzalez/model/Solicitudes.java
+++ src/main/java/cl/duoc/miguel_gonzalez/model/Solicitudes.java
@@ -1,5 +1,26 @@
 package cl.duoc.miguel_gonzalez.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Solicitudes {
 
+    @NotNull(message = "El id no puede tener espacio nulo")
+    private long id;
+
+    @NotBlank(message = "El nombre del paciente debe ser obligatorio")
+    private String paciente;
+
+    @NotNull (message = "Se debe asignar una prioridad")
+    private Integer prioridad;
+
+    @NotBlank(message = "La estado no puede estar vacia")
+    private String estado;
 }
--- src/main/java/cl/duoc/miguel_gonzalez/service/SolicitudesService.java
+++ src/main/java/cl/duoc/miguel_gonzalez/service/SolicitudesService.java
@@ -1,5 +1,25 @@
 package cl.duoc.miguel_gonzalez.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.duoc.miguel_gonzalez.model.Solicitudes;
+import cl.duoc.miguel_gonzalez.repository.SolicitudesRepository;
+
+@Service
 public class SolicitudesService {
 
+    @Autowired
+    private SolicitudesRepository solicitudesRepository;
+
+     public SolicitudesService(SolicitudesRepository solicitudesRepository){
+        this.solicitudesRepository = solicitudesRepository;
+    }
+
+      public List<Solicitudes> getAllProducts(){
+        return solicitudesRepository.findAll();
+    }
+    
 }
